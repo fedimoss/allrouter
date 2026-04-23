f1d32de856353e2348dd91dc146e89408397b977
dingtalk_ygwxpy_fOh <994776685@qq.com>
2026-04-23 10:13:11 +0800

    更改文案，去掉普通用户账单中心菜单

--- web/src/components/layout/headerbar/index.jsx
+++ web/src/components/layout/headerbar/index.jsx
@@ -91,7 +91,7 @@ const HeaderBar = ({ onMobileMenuToggle, drawerOpen }) => {
     '/console/subscription': t('订阅管理'),
     '/console/models': t('模型管理'),
     '/console/deployment': t('模型部署'),
-    '/console/billing': isAdmin() ? t('账单管理') : t('账单中心'),
+    '/console/billing': t('账单中心'),
     '/console/operational': t('运营数据'),
     '/console/user': t('用户管理'),
     '/console/setting': t('系统设置'),
